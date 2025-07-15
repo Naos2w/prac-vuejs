354d612775aa2638b409a000dc586f2a67c6dada
Naos2w <simplelove0922@yahoo.com.tw>
2025-07-15 17:16:09 +0800

    2-vue-component-intro

---
+++ 0-now-project/src/components/Header.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+
+<style scoped>
+header { padding: 1rem; background: #42b983; color: white; text-align: center; }
+</style>
---
+++ 0-now-project/src/components/ItemList.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <ul>
+    <li v-for="item in todos" :key="item.id">
+      <span @click="select(item)">{{ item.title }}</span>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    todos: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    select(item) {
+      this.$emit('item-selected', item)
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul { list-style: none; padding: 0;  }
+li { margin: 0.5rem 0; cursor: pointer; }
+li:hover { text-decoration: underline; }
+</style>
